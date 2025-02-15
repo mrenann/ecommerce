d2075a35cb45b9f36941f98959f2786aceff6f04
mrenann <marcos.renann.br@gmail.com>
2025-02-15 19:18:06 -0300

    feat: multi module configs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,22 @@ dependencyResolutionManagement {
 rootProject.name = "Ecommerce"
 include(":app")
 include(":core")
+include(":navigation")
 include(":feature-onboarding")
 include(":feature-onboarding:domain")
 include(":feature-onboarding:presentation")
-include(":navigation")
+include(":feature-auth")
+//MAIN TABS
+include(":feature-main")
+include(":feature-main:presentation")
+include(":feature-home")
+include(":feature-favorites")
+include(":feature-profile")
+//--
+include(":feature-cart")
+
+include(":feature-home:presentation")
+include(":feature-favorites:presentation")
+include(":feature-cart:presentation")
+include(":feature-profile:presentation")
+include(":feature-auth:presentation")
