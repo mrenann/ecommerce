a62137ac3dff8d618d35712717b47d51b8a57f2c
mrenann <marcos.renann.br@gmail.com>
2025-02-16 10:54:44 -0300

    feat: update person icon to outline in profile tab

--- feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/ProfileTab.kt
+++ feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/ProfileTab.kt
@@ -7,15 +7,15 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import compose.icons.EvaIcons
-import compose.icons.evaicons.Fill
-import compose.icons.evaicons.fill.Person
+import compose.icons.evaicons.Outline
+import compose.icons.evaicons.outline.Person
 
 class ProfileTab : Tab {
     override val options: TabOptions
         @Composable
         get() {
             val title = "Perfil"
-            val icon = rememberVectorPainter(EvaIcons.Fill.Person)
+            val icon = rememberVectorPainter(EvaIcons.Outline.Person)
 
             return remember {
                 TabOptions(
