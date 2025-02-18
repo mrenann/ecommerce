fe647f3e07f8913164c7f9d28f25c4054ae1277c
mrenann <marcos.renann.br@gmail.com>
2025-02-17 23:58:17 -0300

    feat: add CategoriesResponse and getCategories in StoreService

--- core/src/main/java/br/mrenann/core/data/remote/StoreService.kt
+++ core/src/main/java/br/mrenann/core/data/remote/StoreService.kt
@@ -1,6 +1,7 @@
 package br.mrenann.core.data.remote
 
 import br.mrenann.core.data.remote.model.ProductItem
+import br.mrenann.core.data.remote.response.CategoriesResponse
 import br.mrenann.core.data.remote.response.ProductsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -13,4 +14,7 @@ interface StoreService {
     suspend fun getProductById(
         @Path("product_id") id: Int,
     ): ProductItem
+
+    @GET("categories")
+    suspend fun getCategories(): CategoriesResponse
 }
---
+++ core/src/main/java/br/mrenann/core/data/remote/response/CategoriesResponse.kt
@@ -0,0 +1,6 @@
+package br.mrenann.core.data.remote.response
+
+import br.mrenann.core.data.remote.model.Category
+
+
+class CategoriesResponse : ArrayList<Category>()
