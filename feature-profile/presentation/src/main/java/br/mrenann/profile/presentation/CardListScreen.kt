e7856e52267081f4f8bf15cc05b7ce9de2dcb8ad
mrenann <marcos.renann.br@gmail.com>
2025-02-26 21:36:21 -0300

    feat: fetch and display user's card list from Firestore

--- feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/CardListScreen.kt
+++ feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/CardListScreen.kt
@@ -1,12 +1,20 @@
 package br.mrenann.profile.presentation
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,9 +22,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -27,39 +42,38 @@ import com.google.firebase.firestore.firestore
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Outline
 import compose.icons.evaicons.outline.ChevronLeft
-import compose.icons.evaicons.outline.Trash
 import kotlinx.coroutines.tasks.await
 
 data class Card(
     val cardNumber: String = "",
     val cvv: String = "",
-    val expiryDate: String = ""
+    val expiryDate: String = "",
+    val type: String = ""
 )
 
 
-class CardListScreen() : Screen {
+class CardListScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        var cards: List<Card> = emptyList()
+        var cards by remember { mutableStateOf<List<Card>>(emptyList()) }
+
         LaunchedEffect(true) {
             val db = Firebase.firestore
             val userId = Firebase.auth.currentUser?.uid
 
-            if (userId != null) { // Check if cardData is available
+            if (userId != null) {
                 val cardRef =
                     db.collection("users").document(userId).collection("cards").get().await()
                 cards = cardRef.documents.mapNotNull {
                     it.toObject(Card::class.java)
                 }
-            } else {
-                // Handle error: User not logged in or cardData not available
+                Log.i("Firestore", "Fetched Cards: $cards")
             }
         }
 
         Scaffold(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         ) { innerPadding ->
             Column(
                 modifier = Modifier
@@ -70,8 +84,9 @@ class CardListScreen() : Screen {
                 // Header
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 12.dp),
+                    horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     IconButton(onClick = { navigator.pop() }) {
@@ -82,25 +97,71 @@ class CardListScreen() : Screen {
                         )
                     }
                     Text(
+                        modifier = Modifier.weight(1F),
                         text = "Cards",
                         style = MaterialTheme.typography.bodyLarge,
                         fontSize = 18.sp
                     )
-                    IconButton(onClick = { }) {
-                        Icon(
-                            tint = Color.Black,
-                            imageVector = EvaIcons.Outline.Trash,
-                            contentDescription = "Clear Cart"
-                        )
+                }
+
+                // Card List
+                if (cards.isNotEmpty()) {
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        contentPadding = PaddingValues(horizontal = 16.dp)
+                    ) {
+                        itemsIndexed(cards) { index, card ->
+                            CardItem(card = card, index = index)
+                        }
                     }
+                } else {
+                    Text(
+                        text = "No cards available",
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        fontSize = 16.sp,
+                        color = Color.Gray
+                    )
                 }
-
-                Column {
-                    Text("${cards}")
-                }
-
-
             }
+        }
+    }
+
+    @Composable
+    fun CardItem(card: Card, index: Int) {
+        val backgroundColor = if (index % 2 == 0) {
+            Color(0xFFFF7F33)
+        } else {
+            Color(0xFF735DFF)
+        }
+
+        Column(
+            modifier = Modifier
+                .width(200.dp)
+                .height(120.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(backgroundColor)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = card.type,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "**** ${card.cardNumber.takeLast(4)}",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Exp ${card.expiryDate}",
+                fontSize = 14.sp,
+                color = Color.White
+            )
         }
     }
 }
