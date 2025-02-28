a7767bef1eb1e2108ed8c898140c5ca15e3bd8a0
mrenann <marcos.renann.br@gmail.com>
2025-02-27 22:12:00 -0300

    feat: add LazyVerticalGrid to display categories in Categories Screen

--- feature-home/presentation/src/main/java/br/mrenann/home/presentation/screen/CategoriesScreen.kt
+++ feature-home/presentation/src/main/java/br/mrenann/home/presentation/screen/CategoriesScreen.kt
@@ -3,33 +3,44 @@ package br.mrenann.home.presentation.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.mrenann.cart.presentation.WhenArriveScreen
+import br.mrenann.core.domain.model.Category
+import br.mrenann.home.presentation.screenModel.HomeScreenModel
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Outline
 import compose.icons.evaicons.outline.ChevronLeft
@@ -88,71 +99,63 @@ class CategoriesScreen : Screen {
                 )
 
             }
-
-            AddressCard(
-                title = "Send to my address",
-                location = "100-198 W Commercial St, Langston, OK 73050, USA",
-                type = "Residence",
-                navigate = navigateToChooseArrive
-            )
-            AddressCard(
-                title = "Pick up at Ecommerce Agency",
-                location = "224001-224499 E County Rd 345, Mooreland, OK 73852, USA",
-                type = "Agency",
-                navigate = navigateToChooseArrive
-            )
+            val screenModel = koinScreenModel<HomeScreenModel>()
+            val state by screenModel.state.collectAsState()
+            when (state) {
+                is HomeScreenModel.State.Init -> {}
+                is HomeScreenModel.State.Loading -> {}
+                is HomeScreenModel.State.Result -> {
+                    val categories = (state as HomeScreenModel.State.Result).categories
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        horizontalArrangement = Arrangement.spacedBy(6.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.padding(horizontal = 12.dp),
+                    ) {
+                        items(categories.size) { index ->
+                            val category = categories[index]
+                            CategoryCard(
+                                category = category
+                            ) { }
+                        }
+                    }
+                }
+            }
         }
 
     }
 
     @Composable
-    private fun AddressCard(
-        title: String,
-        location: String,
-        type: String,
-        navigate: (String) -> Unit
-    ) {
-        Card(
+    fun CategoryCard(category: Category, onClick: () -> Unit) {
+
+        Column(
             modifier = Modifier
-                .padding(12.dp)
-                .fillMaxWidth(),
-            onClick = { navigate(type) },
-            shape = RoundedCornerShape(4.dp)
+                .height(100.dp)
         ) {
-            Column(modifier = Modifier.background(Color.White)) {
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 12.dp)
-                        .padding(top = 12.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = title,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold
+            Box(contentAlignment = Alignment.BottomStart) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(
+                            category.image
                         )
-                        Text(text = "Free", color = Color(0xFF098D19))
-                    }
-                    Spacer(modifier = Modifier.size(12.dp))
-                    Text(location)
-                    Text(type)
-                }
-                HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
-                Column(
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .padding(horizontal = 12.dp)
-                        .padding(bottom = 12.dp)
-                ) {
-                    Text(
-                        text = "Choose this Address",
-                        color = Color(0xFF2481E8),
-                        fontWeight = FontWeight.Bold
-                    )
-                }
+                        .fillMaxWidth()
+                        .height(120.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+                Text(
+                    text = category.name,
+                    modifier = Modifier.padding(16.dp),
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
             }
+
+
         }
     }
 
@@ -160,6 +163,13 @@ class CategoriesScreen : Screen {
     @Composable
     @Preview
     fun CategoriesContentPreview() {
-        ScreenContent({ false }, {})
+        CategoryCard(
+            category = Category(
+                id = 1,
+                image = "https://imgur.com/ZANVnHE",
+                name = "Electronics"
+            ),
+            onClick = { }
+        )
     }
 }
