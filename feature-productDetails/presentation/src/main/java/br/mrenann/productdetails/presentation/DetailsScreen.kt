cc5790b9184c6bfe6c4f8ad80af13ed278adb391
mrenann <marcos.renann.br@gmail.com>
2025-03-04 17:37:00 -0300

    feat: implement Snackbar and favorite feedback in DetailsScreen

--- feature-productDetails/presentation/src/main/java/br/mrenann/productdetails/presentation/DetailsScreen.kt
+++ feature-productDetails/presentation/src/main/java/br/mrenann/productdetails/presentation/DetailsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +19,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -28,6 +30,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -63,6 +66,7 @@ import compose.icons.evaicons.outline.Bookmark
 import compose.icons.evaicons.outline.ChevronLeft
 import compose.icons.evaicons.outline.Share
 import compose.icons.evaicons.outline.Star
+import kotlinx.coroutines.launch
 
 data class DetailsScreen(
     val id: Int
@@ -76,6 +80,8 @@ data class DetailsScreen(
         var showBottomSheet by remember { mutableStateOf(false) }
         val cartScreenModel = koinScreenModel<CartScreenModel>()
         koinScreenModel<FavoriteScreenModel>()
+        val snackbarHostState = remember { SnackbarHostState() }
+        val coroutineScope = rememberCoroutineScope()
         LaunchedEffect(key1 = id) {
             screenModel.getDetails(DetailsEvent.GetDetails(id))
         }
@@ -84,7 +90,8 @@ data class DetailsScreen(
         Scaffold(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(0xFFF1F1F1))
+                .background(Color(0xFFF1F1F1)),
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
         ) { innerPadding ->
             Column(Modifier.fillMaxSize()) {
 
@@ -141,9 +148,15 @@ data class DetailsScreen(
                                 Row {
                                     IconButton(onClick = {
                                         if (product != null) {
-                                            screenModel.favorite(
-                                                item = product
-                                            )
+                                            screenModel.favorite(item = product)
+
+                                            coroutineScope.launch {
+                                                snackbarHostState.currentSnackbarData?.dismiss()
+                                                snackbarHostState.showSnackbar(
+                                                    message = if (checkedState) "Removed from favorites" else "Added to favorites",
+                                                    duration = SnackbarDuration.Short,
+                                                )
+                                            }
                                         }
 
                                     }) {
