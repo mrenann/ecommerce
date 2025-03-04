c10be0d81d7f319359971dc20640c7693e48e6d4
mrenann <marcos.renann.br@gmail.com>
2025-03-04 11:36:30 -0300

    feat: refactor HomeScreen layout to use a single LazyColumn

--- feature-home/presentation/src/main/java/br/mrenann/home/presentation/screen/HomeScreen.kt
+++ feature-home/presentation/src/main/java/br/mrenann/home/presentation/screen/HomeScreen.kt
@@ -73,174 +73,178 @@ class HomeScreen : Screen {
         val cartScreenModel = koinScreenModel<CartScreenModel>()
         val cartState by cartScreenModel.state.collectAsState()
         cartScreenModel.countItemsFromCart()
-        when (state) {
-            is HomeScreenModel.State.Init -> {}
-            is HomeScreenModel.State.Loading -> {}
-            is HomeScreenModel.State.Result -> {
-                val products = (state as HomeScreenModel.State.Result).products
-                val categories = (state as HomeScreenModel.State.Result).categories
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(
-                                PaddingValues(
-                                    top = innerPadding.calculateTopPadding(),
-                                    start = innerPadding.calculateStartPadding(
-                                        layoutDirection = LayoutDirection.Ltr
-                                    ),
-                                    bottom = 0.dp,
-                                    end = innerPadding.calculateEndPadding(
-                                        layoutDirection = LayoutDirection.Ltr
-                                    ),
-                                )
-                            )
-                            .padding(horizontal = 12.dp),
-                        verticalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        item {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                SectionTitle("Discover")
-                                IconButton(onClick = {
-                                    navigator.push(CartScreen())
-                                }) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        PaddingValues(
+                            top = innerPadding.calculateTopPadding(),
+                            start = innerPadding.calculateStartPadding(
+                                layoutDirection = LayoutDirection.Ltr
+                            ),
+                            bottom = 0.dp,
+                            end = innerPadding.calculateEndPadding(
+                                layoutDirection = LayoutDirection.Ltr
+                            ),
+                        )
+                    )
+                    .padding(horizontal = 12.dp),
+            ) {
+                when (state) {
+                    is HomeScreenModel.State.Init -> {}
+                    is HomeScreenModel.State.Loading -> {}
+                    is HomeScreenModel.State.Result -> {
+                        val products = (state as HomeScreenModel.State.Result).products
+                        val categories = (state as HomeScreenModel.State.Result).categories
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            item {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ) {
+                                    SectionTitle("Discover")
+                                    IconButton(onClick = {
+                                        navigator.push(CartScreen())
+                                    }) {
 
-                                    BadgedBox(
-                                        badge = {
-                                            if (cartState is CartScreenModel.State.Result) {
-                                                val cartItemCount =
-                                                    (cartState as CartScreenModel.State.Result).state.itemsCount
-                                                if (cartItemCount > 0) { // Exibir apenas se houver itens no carrinho
-                                                    Badge(
-                                                        containerColor = Color(0xFF1373FF)
-                                                    ) {
-                                                        Text(text = cartItemCount.toString())
+                                        BadgedBox(
+                                            badge = {
+                                                if (cartState is CartScreenModel.State.Result) {
+                                                    val cartItemCount =
+                                                        (cartState as CartScreenModel.State.Result).state.itemsCount
+                                                    if (cartItemCount > 0) { // Exibir apenas se houver itens no carrinho
+                                                        Badge(
+                                                            containerColor = Color(0xFF1373FF)
+                                                        ) {
+                                                            Text(text = cartItemCount.toString())
+                                                        }
                                                     }
                                                 }
-                                            }
 
+                                            }
+                                        ) {
+                                            Icon(
+                                                tint = Color.Black,
+                                                imageVector = EvaIcons.Outline.ShoppingCart,
+                                                contentDescription = "Shopping Cart",
+                                            )
                                         }
+                                    }
+                                }
+                            }
+
+                            item {
+                                SearchBar(query = searchQuery, onQueryChange = { searchQuery = it })
+                            }
+
+                            item {
+                                Box(
+                                    modifier = Modifier
+                                        .padding(vertical = 12.dp)
+                                        .background(Color(0xFF3B73FF), RoundedCornerShape(8.dp))
+                                        .fillMaxWidth()
+                                        .height(150.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                ) {
+                                    AsyncImage(
+                                        modifier = Modifier.background(
+                                            Color.Black,
+                                            RoundedCornerShape(8.dp)
+                                        ),
+                                        model = ImageRequest.Builder(LocalContext.current)
+                                            .data("https://github.com/mrenann/mrenann/blob/master/img/1.png?raw=true")
+                                            .crossfade(true)
+                                            .build(),
+                                        contentDescription = "",
+                                        contentScale = ContentScale.FillWidth,
+                                    )
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(horizontal = 8.dp, vertical = 14.dp)
+                                            .fillMaxWidth(.5F)
+                                            .fillMaxHeight(),
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        verticalArrangement = Arrangement.Center
                                     ) {
-                                        Icon(
-                                            tint = Color.Black,
-                                            imageVector = EvaIcons.Outline.ShoppingCart,
-                                            contentDescription = "Shopping Cart",
+                                        Text(
+                                            style = MaterialTheme.typography.bodyLarge,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 26.sp,
+                                            text = "Buy your electronics",
+                                            color = Color.White
                                         )
                                     }
                                 }
                             }
-                        }
 
-                        item {
-                            SearchBar(query = searchQuery, onQueryChange = { searchQuery = it })
-                        }
+                            item { SectionTitle("Categories") }
 
-                        item {
-                            Box(
-                                modifier = Modifier
-                                    .padding(vertical = 12.dp)
-                                    .background(Color(0xFF3B73FF), RoundedCornerShape(8.dp))
-                                    .fillMaxWidth()
-                                    .height(150.dp)
-                                    .clip(RoundedCornerShape(8.dp))
-                            ) {
-                                AsyncImage(
-                                    modifier = Modifier.background(
-                                        Color.Black,
-                                        RoundedCornerShape(8.dp)
-                                    ),
-                                    model = ImageRequest.Builder(LocalContext.current)
-                                        .data("https://github.com/mrenann/mrenann/blob/master/img/1.png?raw=true")
-                                        .crossfade(true)
-                                        .build(),
-                                    contentDescription = "",
-                                    contentScale = ContentScale.FillWidth,
-                                )
-                                Column(
-                                    modifier = Modifier
-                                        .padding(horizontal = 8.dp, vertical = 14.dp)
-                                        .fillMaxWidth(.5F)
-                                        .fillMaxHeight(),
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.Center
-                                ) {
-                                    Text(
-                                        style = MaterialTheme.typography.bodyLarge,
-                                        fontWeight = FontWeight.Bold,
-                                        fontSize = 26.sp,
-                                        text = "Buy your electronics",
-                                        color = Color.White
-                                    )
-                                }
-                            }
-                        }
-
-                        item { SectionTitle("Categories") }
-
-                        item {
-                            LazyVerticalGrid(
-                                columns = GridCells.Fixed(3),
-                                horizontalArrangement = Arrangement.spacedBy(12.dp),
-                                verticalArrangement = Arrangement.spacedBy(8.dp),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .heightIn(
-                                        min = 100.dp,
-                                        max = 240.dp
-                                    ), // Set a fixed height here
-                                userScrollEnabled = false
-                            ) {
-                                val modifiedCategories = categories.take(5).toMutableList().apply {
-                                    if (categories.size > 5) add(
-                                        Category(
-                                            id = 0,
-                                            image = "",
-                                            name = "More",
-                                        )
-                                    )
-                                }
-                                items(modifiedCategories.size) { index ->
-                                    val category = modifiedCategories[index]
-                                    CategoryCard(category = category.name, onClick = {
-                                        if (category.id == 0) navigatorChildren.push(
-                                            CategoriesScreen()
-                                        ) else navigatorChildren.push(
-                                            CategoryScreen(category = category)
-                                        )
-                                    })
-                                }
-                            }
-                        }
-
-                        item { SectionTitle("For You") }
-
-                        item {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .heightIn(min = 100.dp, max = 1000.dp)
-                            ) {
+                            item {
                                 LazyVerticalGrid(
-                                    columns = GridCells.Fixed(2),
-                                    userScrollEnabled = false,
+                                    columns = GridCells.Fixed(3),
                                     horizontalArrangement = Arrangement.spacedBy(12.dp),
                                     verticalArrangement = Arrangement.spacedBy(8.dp),
-                                    modifier = Modifier.fillMaxWidth()
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .heightIn(
+                                            min = 100.dp,
+                                            max = 240.dp
+                                        ), // Set a fixed height here
+                                    userScrollEnabled = false
                                 ) {
-                                    val productsSize =
-                                        if (products.size > 8) 8 else products.size // Limit to 8 items
-                                    items(productsSize) { index -> ProductCard(products[index]) }
+                                    val modifiedCategories =
+                                        categories.take(5).toMutableList().apply {
+                                            if (categories.size > 5) add(
+                                                Category(
+                                                    id = 0,
+                                                    image = "",
+                                                    name = "More",
+                                                )
+                                            )
+                                        }
+                                    items(modifiedCategories.size) { index ->
+                                        val category = modifiedCategories[index]
+                                        CategoryCard(category = category.name, onClick = {
+                                            if (category.id == 0) navigatorChildren.push(
+                                                CategoriesScreen()
+                                            ) else navigatorChildren.push(
+                                                CategoryScreen(category = category)
+                                            )
+                                        })
+                                    }
                                 }
                             }
 
-                        }
+                            item { SectionTitle("For You") }
 
+                            item {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .heightIn(min = 100.dp, max = 1000.dp)
+                                ) {
+                                    LazyVerticalGrid(
+                                        columns = GridCells.Fixed(2),
+                                        userScrollEnabled = false,
+                                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                                        modifier = Modifier.fillMaxWidth()
+                                    ) {
+                                        val productsSize =
+                                            if (products.size > 8) 8 else products.size // Limit to 8 items
+                                        items(productsSize) { index -> ProductCard(products[index]) }
+                                    }
+                                }
+
+                            }
+
+                        }
                     }
                 }
             }
