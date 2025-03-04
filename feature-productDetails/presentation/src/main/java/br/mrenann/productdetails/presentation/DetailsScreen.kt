06e3dd10bf32de328e3b65673996f49ebb01b2c7
mrenann <marcos.renann.br@gmail.com>
2025-03-04 20:16:19 -0300

    feat: update "Add to Cart" button in DetailsScreen to use blue color and reflect action

--- feature-productDetails/presentation/src/main/java/br/mrenann/productdetails/presentation/DetailsScreen.kt
+++ feature-productDetails/presentation/src/main/java/br/mrenann/productdetails/presentation/DetailsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -291,6 +292,9 @@ data class DetailsScreen(
                             Button(
                                 modifier = Modifier.weight(1F),
                                 shape = RoundedCornerShape(10.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF1373FF)
+                                ),
                                 onClick = {
                                     if (product != null) cartScreenModel.addProduct(product = product.toProductCart())
                                     showBottomSheet = true
