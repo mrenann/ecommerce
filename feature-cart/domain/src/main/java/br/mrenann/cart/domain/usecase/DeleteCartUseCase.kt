b01ea2d08bd5aedbe8210fedd46c8be804d50759
mrenann <marcos.renann.br@gmail.com>
2025-03-04 21:50:18 -0300

    refactor: change DeleteCartUseCase parameter from Product to ProductCart

--- feature-cart/domain/src/main/java/br/mrenann/cart/domain/usecase/DeleteCartUseCase.kt
+++ feature-cart/domain/src/main/java/br/mrenann/cart/domain/usecase/DeleteCartUseCase.kt
@@ -1,9 +1,10 @@
 package br.mrenann.cart.domain.usecase
 
 import br.mrenann.core.domain.model.Product
+import br.mrenann.core.domain.model.ProductCart
 import kotlinx.coroutines.flow.Flow
 
 interface DeleteCartUseCase {
     suspend operator fun invoke(params: Params): Flow<Unit>
-    data class Params(val item: Product)
+    data class Params(val item: ProductCart)
 }
