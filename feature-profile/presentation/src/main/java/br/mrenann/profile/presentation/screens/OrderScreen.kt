654b2947822a82b01044cfb267bc29f147718d77
mrenann <marcos.renann.br@gmail.com>
2025-03-06 16:39:02 -0300

    feat: order details

--- feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/screens/OrderScreen.kt
+++ feature-profile/presentation/src/main/java/br/mrenann/profile/presentation/screens/OrderScreen.kt
@@ -1,15 +1,65 @@
 package br.mrenann.profile.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import br.mrenann.core.domain.model.Order
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import compose.icons.EvaIcons
+import compose.icons.evaicons.Outline
+import compose.icons.evaicons.outline.ChevronLeft
 
 data class OrderScreen(
     val order: Order
 ) : Screen {
     @Composable
     override fun Content() {
-        Text(text = "Order Details")
+        val navigator = LocalNavigator.currentOrThrow
+
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(onClick = { navigator.pop() }) {
+                        Icon(
+                            imageVector = EvaIcons.Outline.ChevronLeft,
+                            contentDescription = "Back"
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.weight(1F),
+                        text = "Order Details",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontSize = 18.sp
+                    )
+                }
+
+
+            }
+        }
     }
 }
